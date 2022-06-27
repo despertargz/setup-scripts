5ea6ebf08e23238d302518d869be54c672c6d834
chris <chris@DESKTOP-CVAGRB0.localdomain>
2022-06-27 10:02:25 -0500

    scripts

---
+++ apt-install.sh
@@ -0,0 +1,13 @@
+apt-add-repository ppa:fish-shell/release-3
+add-apt-repository ppa:neovim-ppa/stable 
+
+apt update
+
+apt install -y \
+python3-pip \
+fish \
+neovim \
+ripgrep \
+fd-find \
+bat \
+exa 
---
+++ pipenv.sh
@@ -0,0 +1,2 @@
+pip install pipenv
+pip install --upgrade --user setuptools==58.3.0
